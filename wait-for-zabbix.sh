95fa40f234d609378b7ff33cba8260d148db8e6b
Douglas Weide <88517406+weidedouglas@users.noreply.github.com>
2024-12-08 17:34:23 -0300

    Update wait-for-zabbix.sh

--- wait-for-zabbix.sh
+++ wait-for-zabbix.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Aguarde até o Zabbix estar pronto para aceitar conexões
-#while ! curl --silent --fail http://localhost:8080; do
- #   echo "Aguardando Zabbix iniciar..."
-#    sleep 5
-#done
-
-# Comando PHP a ser executado após o Zabbix estar up
-# php /usr/share/zabbix/web/generate_token.php
+while ! curl -sf http://localhost/zabbix; do
+    echo "Waiting for Zabbix to start..."
+    sleep 5
+done
 php /usr/share/zabbix/web/generate_token.php
 
