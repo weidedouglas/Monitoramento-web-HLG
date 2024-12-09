88f1d8050047ad3f3998f201ce631396ce217099
Douglas Weide <88517406+weidedouglas@users.noreply.github.com>
2024-12-08 21:30:26 -0300

    Create create.group.php

---
+++ create.group.php
@@ -0,0 +1,41 @@
+<?php
+
+$curl = curl_init();
+
+// Specify the file path
+$filePath = 'zabbix_token.txt';
+  
+// Check if the file exists
+if (!file_exists($filePath)) {
+    die("Error: File does not exist.");
+}   
+    
+// Read the file content
+$tokenAPI = file_get_contents($filePath);
+
+
+curl_setopt_array($curl, [
+  CURLOPT_PORT => "8080",
+  CURLOPT_URL => "http://localhost:8080/api_jsonrpc.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n           \"jsonrpc\": \"2.0\",\n           \"method\": \"hostgroup.create\",\n           \"params\": {\n               \"name\": \"Grupo Teste 1\"\n           },\n\t\t\t\t\t \"auth\": \"$tokenAPI\",\n           \"id\": 1\n }",
+  CURLOPT_HTTPHEADER => [
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
